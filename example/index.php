6f3f1bf9ae0949cfa272e3d92ebf5b16d6ba3112
Mark <chumheramis@gmail.com>
2014-10-16 20:09:43 +0800

    Create index.php

---
+++ example/index.php
@@ -0,0 +1,7 @@
+<link href="http://localhost/Work/work.CMS.prototype/core/EasyDB/example/style.css" rel="stylesheet" type="text/css"/>
+<?php
+
+include "../class-EasyDB.php";
+include "1.connectdb.php";
+include "2.dbcontentmanipulation.php";
+include "3.getdatafromtable.php";
